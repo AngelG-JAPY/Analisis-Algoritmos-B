e280a4d8680bc0aa707686e9b0d5037f4f27448c
AngelG-JAPY <angelgarciara16@gmail.com>
2022-04-04 07:38:46 -0500

    Terminados los algoritmos recursivos propuestos y calculados los costes. Falta verificar si está bien. :)

--- Algoritmos/src/Recurrencia.java
+++ Algoritmos/src/Recurrencia.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
 
-import javax.lang.model.element.Element;
-
 /**
  * Recurrencia
  */
@@ -11,31 +9,102 @@ public class Recurrencia {
         if (b == 1)
             return a;
         return a + recursiveAdd(a, b - 1);
+
+        /**
+         * -------------
+         * T(n) = | c, b == 1
+         * | T(n-1) + c
+         * --------------
+         * T(n) = T(n-1) + c;
+         * = T(n-2) + c + c = T(n-2) + 2c
+         * = (T(n-3) + c) + 2c
+         * = T(n-3) + 3c
+         * = .
+         * = .
+         * = .
+         * = T(n-(n-1)) + (n-1)c
+         * = T(1) + (n-1)c
+         * = c + cn - c
+         * T(n) = cn
+         * O(n)
+         * 
+         */
     }
 
     public static int recursiveDivision(int a, int b) {
-        if (b == 0)
-            return -1;
         if (b > a)
             return 0;
         return 1 + recursiveDivision(a - b, b);
+        /*
+         * --------------
+         * T(a,b) | c, b > a
+         * | T(a-b) + c
+         * ---------------
+         * 
+         * T(a,b) = T(a-b) + c
+         * = (T((a-b)-b) + c) + c
+         * = T(a- 2b) + 2c
+         * = (T((a-2b)-b) + c) + c
+         * = T(a-3b) + 3c
+         * = ........
+         * Se tiene que b > a para retornar c, luego a/b < a
+         * = T(a/b) + a/bc
+         * = T(1) + (a/b)c
+         * = c + ac/b
+         * T(a,b) = a/b
+         * O(a/b)
+         */
     }
 
     public static int recursiveSumOfElements(int A[], int n) {
         if (n == 0)
             return A[0];
         return A[n - 1] + recursiveSumOfElements(A, n - 1);
+        /*
+         * --------------
+         * T(n) = | c, n = 0
+         * | T(n-1) + c
+         * ---------------
+         * 
+         * T(n) = T(n-1) + c
+         * = (T(n-2) + c) + c
+         * = (T(n-3) + c) + 2c
+         * = (T(n-4) + c) + 3c
+         * = T(n-4) + 4c
+         * = ................
+         * = T(n-(n-1)) + (n-1)c
+         * = T(1) + (n-1)c
+         * = c + cn - c
+         * T(n) = cn
+         * O(n)
+         */
     }
 
     // ii. Encontrar la posición del mayor valor en el array
-    public static int largestElement(int array[], int element, int position, int length) {
-        if (position < length) {
-            if (element <= array[position]) {
-                element = array[position];
-            }
-            element = largestElement(array, element, position + 1, length);
-        }
-        return element;
+    public static int largestElement(int array[], int element, int length) {
+        if (length == 0)
+            return element;
+        if (element <= array[length-1])
+            element = array[length-1];
+        return largestElement(array,element,length-1);
+        /*
+         *         --------------
+         * T(n) = | c, n = 0
+         *        | T(n-1) + c
+         *        ---------------
+         * 
+         * T(n) = T(n-1) + c
+         *      = (T(n-2) + c) + c
+         *      = (T(n-3) + c) + 2c
+         *      = (T(n-4) + c) + 3c
+         *      = T(n-4) + 4c
+         *      = ................
+         *      = T(n-(n-1)) + (n-1)c
+         *      = T(1) + (n-1)c
+         *      = c + cn - c
+         *      T(n) = cn
+         *      O(n)
+         */
     }
 
     // iii. Invertir el array sobre el mismo
@@ -53,8 +122,28 @@ public class Recurrencia {
 
             recursiveInvertArray(A, length, position + 1);
         }
-
         return A;
+         /*
+         *         --------------
+         * T(n) = | c, n < n/2
+         *        | T(n+1) + c
+         *        ---------------
+         * 
+         * T(n) = T(n+1) + c
+         *      = (T(n+2) + c) + c
+         *      = (T(n+3) + c) + 2c
+         *      = (T(n+4) + c) + 3c
+         *      = T(n+4) + 4c
+         *      = ................
+         *      = T(n+(n+1)) + (n+1)c
+         *      pero n < n/2 podemos decir que n == n
+         *      = T(n) + (n+1)c
+         * T(n) = c + (n+1)c
+         * T(n) = c + cn + c
+         * T(n) = cn + 2c
+         * T(n) = cn + c
+         * O(n)
+         */
     }
 
     // iv. Encontrar el primer índice tal que A[i] sea igual a i
@@ -82,20 +171,20 @@ public class Recurrencia {
         return l;
     }
 
-
     public static void main(String[] args) {
         // System.out.println(recursiveAdd(5, 2));
         // System.out.println(recursiveDivision(15, 4));
-        // int[] array = { 5, 10, 2, 5, 78, 5, 8, 250, 11, 10, 25, 69, 30, 154, 15 };
+        int[] array = { 5, 10, 2, 5, 78, 5, 8, 250, 11, 10, 25, 69, 30, 154, 15 };
         // System.out.println(recursiveSumOfElements(array, array.length));
-        // System.out.println(largestElement(array, array[0], 0, array.length));
+        System.out.println(largestElement(array, array[0],array.length));
         // int a[] = recursiveInvertArray(array, array.length , 0);
         // for(int i: a ) System.out.print(i+", ");
         // int array2[] = { 3, 5, 5, 4, 4 };
         // System.out.println(firstMatch(array2, array2.length, 0));
-        int a[] = { 1, 2, 3, 5, 7 };
-        int b[] = { 5, 3, 2 };
-        // System.out.println(lol(a, b));
-        System.out.println(recursiveIntersection(a, b, a.length, b.length, 0, 0, new ArrayList<Integer>()));
+        // int a[] = { 1, 2, 3, 5, 7 };
+        // int b[] = { 5, 3, 2 };
+        // // System.out.println(lol(a, b));
+        // System.out.println(recursiveIntersection(a, b, a.length, b.length, 0, 0, new
+        // ArrayList<Integer>()));
     }
 }
